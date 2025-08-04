611367b935f611b3170d38eeb021e32ebaaf4567
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-08-04 22:39:43 +0200

    Test static sky

--- lua/main.lua
+++ lua/main.lua
@@ -9,8 +9,14 @@ function _update60()
 end
 
 function _draw()
-  cls()
-  map()
+  cls(1)
+  camera()
+  fillp(▤)
+  rectfill(0, 0, 128, 26, 0)
+  fillp()
+  rectfill(0, 0, 128, 20, 0)
+
   camera(max(min(player.x - 64, 48), 0), player.y - 64)
+  map()
   player:draw()
 end
