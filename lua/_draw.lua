c314d5b566fbc33bfd1299b3a16633691f5a242a
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-08-05 20:54:45 +0200

    Gfx tweaks

--- lua/_draw.lua
+++ lua/_draw.lua
@@ -1,6 +1,7 @@
 function _draw()
   camera()
 
+  -- sky
   cls(1)
   fillp(▤)
   rectfill(0, 0, 128, 24, 0)
@@ -13,20 +14,25 @@ function _draw()
   fillp()
   rectfill(0, 118, 128, 128, 8)
 
+  -- stars
   circfill(10, 10, .5, 7)
   circfill(16, 22, .5, 7)
   circfill(36, 32, 1, 7)
   circfill(52, 8, 1, 7)
   circfill(56, 16, .5, 7)
+  circfill(82, 18, 1, 7)
   circfill(94, 16, .5, 7)
   circfill(110, 20, .5, 7)
 
+  -- sun
   circfill(98, 102, 14, 8)
   circfill(98, 102, 12, 2)
   circfill(98, 102, 10, 15)
   circfill(98, 102, 8, 7)
 
-  camera(max(min(player.x - 64, 48), 0), player.y - 64)
+  print(stat(9), 0, 0, 7)
+
+  camera(max(min(player.x - 64, 48), 0), min(player.y - 72, 16))
 
   map()
   player:draw()
