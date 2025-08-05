646a405619ac2cb4b7e2fd64ce5bbedd59b3a268
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-08-05 12:24:20 +0200

    Tweak sky

--- lua/_draw.lua
+++ lua/_draw.lua
@@ -4,11 +4,15 @@ function _draw()
 
   fillp(▤)
   rectfill(0, 0, 128, 26, 0)
+  rectfill(0, 110, 128, 128, 2)
   fillp()
   rectfill(0, 0, 128, 20, 0)
+  rectfill(0, 116, 128, 128, 2)
+  circfill(104, 24, 10, 15)
+  circfill(104, 24, 8, 7)
 
   camera(max(min(player.x - 64, 48), 0), player.y - 64)
-  map()
 
+  map()
   player:draw()
 end
