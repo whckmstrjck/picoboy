ffbdcb61e2209f3e11b60dc7ecd09d20a167d36a
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-09-07 23:12:28 +0200

    Snyggify SmallExplosion particle

--- lua/classes/particles/small-explosion.lua
+++ lua/classes/particles/small-explosion.lua
@@ -5,10 +5,16 @@ SmallExplosion = Particle:new({
   end,
   draw = function(_ENV)
     local r = t / ttl * radius
+    local ll = (ttl - t) / ttl * 9
+
+    line(x - ll, y, x + ll, y, 7)
+    line(x, y - ll, x, y + ll, 7)
 
     circ(x, y, r + 3, 6)
+    -- circ(x, y, r + 4, 6)
 
     circfill(x, y, r, 15)
-    circfill(x, y, r - 2, 7)
+    circfill(x, y, r - 1, 7)
+    circfill(x, y, r - 3, 0)
   end
 })
