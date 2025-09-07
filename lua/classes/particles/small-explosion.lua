c239b2077ed930699eb90d95571d3564e69ebb5a
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-09-07 22:58:35 +0200

    Add small explosion particle for when enemies die

--- lua/classes/particles/small-explosion.lua
+++ lua/classes/particles/small-explosion.lua
@@ -1,9 +1,14 @@
 SmallExplosion = Particle:new({
   ttl = 12,
-
+  radius = 6,
   process_particle = function(_ENV)
   end,
   draw = function(_ENV)
-    circfill(x, y, 1 + flr(t / 4), 8)
+    local r = t / ttl * radius
+
+    circ(x, y, r + 3, 6)
+
+    circfill(x, y, r, 15)
+    circfill(x, y, r - 2, 7)
   end
 })
