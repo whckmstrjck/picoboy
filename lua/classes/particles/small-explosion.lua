b814ec9c3c8c3998a8346b8bacbb826b04030a43
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-09-08 07:25:05 +0200

    Improve SmallExplosion

--- lua/classes/particles/small-explosion.lua
+++ lua/classes/particles/small-explosion.lua
@@ -1,20 +1,23 @@
 SmallExplosion = Particle:new({
-  ttl = 12,
+  ttl = 10,
   radius = 6,
   process_particle = function(_ENV)
   end,
   draw = function(_ENV)
     local r = t / ttl * radius
-    local ll = (ttl - t) / ttl * 9
+    local ll = (ttl - t) / ttl * 12
+    local ll2 = ll * .64
 
     line(x - ll, y, x + ll, y, 7)
     line(x, y - ll, x, y + ll, 7)
+    rectfill(x - ll2, y - 1, x + ll2, y + 1, 7)
+    rectfill(x - 1, y - ll2, x + 1, y + ll2, 7)
 
-    circ(x, y, r + 3, 6)
+    circ(x, y, r + 3, 7)
     -- circ(x, y, r + 4, 6)
 
     circfill(x, y, r, 15)
     circfill(x, y, r - 1, 7)
-    circfill(x, y, r - 3, 0)
+    circfill(x, y, r - 2, 0)
   end
 })
